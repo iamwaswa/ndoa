8b6d8ed053f4722fda3188e676588285b985a2a9
iamwaswa <olungaw@gmail.com>
2021-02-06 21:20:33 -0800

    Update carousel and font

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,5 @@
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { AppProps } from 'next/app';
 import CssBaseline from '@material-ui/core/CssBaseline';
