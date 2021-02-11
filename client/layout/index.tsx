303a2616ca0316044c6d1a69f2903e57c1984409
iamwaswa <olungaw@gmail.com>
2021-02-10 23:17:19 -0800

    Fade in layout header

--- client/layout/index.tsx
+++ client/layout/index.tsx
@@ -11,19 +11,22 @@ import { Logo } from './logo';
 import { Names } from './names';
 import { Navigation } from './navigation';
 import { WeddingDate } from './weddingDate';
+import Fade from '@material-ui/core/Fade';
 
 export function Layout({ children }: ChildrenProps): JSX.Element {
   return (
     <LayoutContainer as="main">
-      <LayoutHeader as="header">
-        <Logo />
-        <Names />
-        <LayoutMiddleContainer>
-          <WeddingDate />
-          <Countdown />
-        </LayoutMiddleContainer>
-        <Navigation />
-      </LayoutHeader>
+      <Fade in={true}>
+        <LayoutHeader as="header">
+          <Logo />
+          <Names />
+          <LayoutMiddleContainer>
+            <WeddingDate />
+            <Countdown />
+          </LayoutMiddleContainer>
+          <Navigation />
+        </LayoutHeader>
+      </Fade>
       <LayoutContent as="section">{children}</LayoutContent>
     </LayoutContainer>
   );
