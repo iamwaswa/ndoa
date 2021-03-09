8b2d6e327cf482129e05ed8976410a2dec06bf84
iamwaswa <olungaw@gmail.com>
2021-03-08 20:02:02 -0800

    Fix registry item button

--- client/components/registryItem/action/styles/index.tsx
+++ client/components/registryItem/action/styles/index.tsx
@@ -16,12 +16,14 @@ interface IRegistryItemButtonProps extends FabProps {
 export const RegistryItemButton = styled(Fab)<IRegistryItemButtonProps>`
   ${({ invert = false, theme }) => css`
     background-color: ${invert ? `var(--gold)` : `var(--dark-blue)`};
+    border-radius: ${theme.spacing(3)}px;
     color: ${invert ? `var(--dark-blue)` : `var(--gold)`};
     font-display: var(--title-font-display);
     font-family: var(--title-font-family);
     font-size: ${theme.typography.h6.fontSize};
     font-style: var(--title-font-style);
     font-weight: var(--title-font-weight);
+    padding: ${theme.spacing(0, 2)};
     text-transform: initial;
   `}
 `;
